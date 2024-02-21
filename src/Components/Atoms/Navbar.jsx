b093319b5704ad0592cf1a69d622c7c879fea189
didi <didier.mendoza.uni@gmail.com>
2024-02-20 23:43:06 -0600

    Cambios didi

---
+++ src/Components/Atoms/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import img2 from '../../assets/img/ajuste.png';
+import Search from './Search';
+import './Navbar.css';
+
+function Navbar() {
+    return ( 
+        <div className="container-nav">
+            <div className="logo">
+                <span className="logo-text">Campus<span className="logo-link">Link</span></span>
+            </div>
+            <div className="search-container">
+                <Search /> 
+            </div>
+            <div className="nav-links">
+                <button className="nav-button">Inicio</button>
+                <button className="nav-button">Grupos</button>
+                <button className="nav-button">Artículos</button>
+                <button className="nav-button">Comunicados</button>
+            </div>
+            <div className="profile-settings">
+                <button className="nav-button nav-link-right">Mi perfil</button>
+                <div className="profile-img-container">
+                    <button className="nav-button"><img className="ajuste-img" src={img2} alt="Ajustes" /></button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
